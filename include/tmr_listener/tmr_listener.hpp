0148fa322c73789878070f0064ac9791ddd0cdc1
jacky.tseng <jacky.tseng@gyro.com.tw>
2021-06-25 21:08:23 +0800

    Create plugin manager, and minor changes
    - test file link to gtest_main, remove test_main.cpp

--- include/tmr_listener/tmr_listener.hpp
+++ include/tmr_listener/tmr_listener.hpp
@@ -2,6 +2,7 @@
 #define tmr_listener_HPP_
 
 #include <boost/shared_ptr.hpp>
+#include <type_traits>
 
 #include "tmr_listener/tmr_listener_handle.hpp"
 #include "tmr_tcp_comm/tmr_tcp_comm.hpp"
@@ -12,24 +13,133 @@
 
 namespace tmr_listener {
 
+using TMTaskHandler        = boost::shared_ptr<ListenerHandle>;
+using TMTaskHandlerArray_t = std::vector<TMTaskHandler>;
+
+/**
+ * @brief Interface of TMR plugin manager, inherit from this to make your custom plugin manager
+ */
+struct TMRPluginManagerBase {
+  /**
+   * @brief Get all plugins
+   *
+   * @return TMTaskHandlerArray_t
+   */
+  virtual TMTaskHandlerArray_t get_all_plugins() const = 0;
+
+  /**
+   * @brief   This function finds the listner handle that is willing to take on task according to input string
+   *
+   * @param  t_input  Enter node message sent from TM robot listen node
+   * @return TMTaskHandler that accepted the test
+   *
+   * @note   This function doesn't catch any exception. Implementer should handle exception within the function
+   *         . Also, empty shared_ptr is not checked, implementer should return their own default handler if non is
+   *         matched.
+   */
+  virtual TMTaskHandler find_task_handler(std::string const &t_input) const = 0;
+
+  TMRPluginManagerBase()                                        = default;
+  TMRPluginManagerBase(TMRPluginManagerBase const & /*unused*/) = default;
+  TMRPluginManagerBase(TMRPluginManagerBase && /*unused*/)      = default;
+
+  TMRPluginManagerBase &operator=(TMRPluginManagerBase const & /*unused*/) = default;
+  TMRPluginManagerBase &operator=(TMRPluginManagerBase && /*unused*/) = default;
+
+  virtual ~TMRPluginManagerBase() = default;
+};
+
+/**
+ * @brief Default handler used by RTLibPluginManager
+ */
+class ScriptExitHandler final : public ListenerHandle {
+ protected:
+  MessagePtr generate_cmd(MessageStatus /*unused*/) noexcept override {
+    using namespace motion_function;
+    return TMSCT << ID{"TMRobotListener_DefaultHandler"} << ScriptExit();
+  }
+
+  Decision start_task(std::string const & /*unused*/) noexcept override { return Decision::Ignore; }
+};
+
+/**
+ * @brief Default plugin manager used by TMRobotListener, it loads plugin via pluginlib. If no loaded plugin accept
+ *        incoming response when enter listen node, it will return default handler, which generates ScriptExit().
+ */
+class RTLibPluginManager final : public TMRPluginManagerBase {
+  TMTaskHandlerArray_t load_plugins();
+
+  pluginlib::ClassLoader<ListenerHandle> class_loader_{"tmr_listener", "tmr_listener::ListenerHandle"};
+  TMTaskHandlerArray_t all_plugins_{load_plugins()};
+  TMTaskHandler default_handler_{boost::make_shared<ScriptExitHandler>()};
+
+ public:
+  TMTaskHandlerArray_t get_all_plugins() const noexcept override { return this->all_plugins_; }
+  TMTaskHandler find_task_handler(std::string const &) const noexcept override;
+};
+
+using TMRPluginManagerBasePtr = boost::shared_ptr<TMRPluginManagerBase>;
+
+/**
+ * @brief This class integrates (1) message parsing, (2) communication and (3) plugin management together.
+ *
+ * @details Default behaviour of plugin management uses pluginlib for plugin management to separate library code from
+ *          application code, you can provide your own plugin management by inheriting TMRPluginManagerBase if pluginlib
+ *          is an overkill, e.g. you only need one plugin.
+ *
+ *          Otherwise, just include `tmr_listener.launch`, or put `<node pkg="tmr_listener" type="tmr_listener_node"
+ *          name="tmr_listener" args="$(arg ip_arg)"/>` in your launch file and you're good to go.
+ */
 class TMRobotListener {
+ public:
+  using ListenData          = boost::variant<TMSCTPacket, TMSTAPacket, CPERRPacket>;
+  using TMRListenDataParser = ParseRule<ListenData>;
+
+  /**
+   * @brief This function parses input string into one of the packets, i.e., TMSCT, TMSTA, or CPERR
+   *
+   * @param t_input string to parse
+   * @return ListenData
+   *
+   * @todo throw if not fully matched in the future
+   */
+  static ListenData parse(std::string t_input);
+
+  /**
+   * @brief This function returns the parsing rule of listen message
+   *
+   * @return TMRListenDataParser
+   */
+  static TMRListenDataParser parsing_rule();
+
+  static constexpr auto LISTENER_PORT = 5890;
+
+  /**
+   * @brief Construct a new TMRobotListener object
+   *
+   * @param t_ip_addr The ip address of the TM robot
+   * @param t_plugin_manager  The ListenerHandle plugin manager, default uses plugin lib to load plugins
+   */
+  TMRobotListener(std::string const &t_ip_addr,
+                  TMRPluginManagerBasePtr const &t_plugin_manager = boost::make_shared<RTLibPluginManager>()) noexcept
+    : tcp_comm_{TMRobotTCP::Callback{boost::bind(&TMRobotListener::receive_tm_msg_callback, this, _1),
+                                     boost::bind(&TMRobotListener::finished_transfer_callback, this, _1),
+                                     boost::bind(&TMRobotListener::disconnected_callback, this)},
+                LISTENER_PORT, t_ip_addr},
+      plugin_manager_{t_plugin_manager} {}
+
+  /**
+   * @brief This function is the entry point to the TCP/IP connection, it initiates the thread loop and runs io services
+   *        in the background
+   */
+  void start();
+
  private:
-  class ScriptExitHandler final : public ListenerHandle {
-   protected:
-    MessagePtr generate_cmd(MessageStatus /*unused*/) override {
-      using namespace motion_function;
-      return TMSCT << ID{"TMRobotListener_DefaultHandler"} << ScriptExit();
-    }
-
-    Decision start_task(std::string const & /*unused*/) override { return Decision::Ignore; }
-  };
-
-  using TMTaskHandler        = boost::shared_ptr<ListenerHandle>;
-  using TMTaskHandlerArray_t = std::vector<TMTaskHandler>;
-
   static constexpr auto TMR_INIT_MSG_ID = "0"; /* !< TM robot message id when first enter listen node */
 
-  TMRobotTCP tcp_comm_; /*!< TM TCP communication object */
+  TMRobotTCP tcp_comm_;                    /*!< TM TCP communication object */
+  TMRPluginManagerBasePtr plugin_manager_; /*!< TM plugin manager holder */
+  TMTaskHandler current_task_handler_{};   /*!< plugin that is currently handling incoming packet */
 
   /**
    * @brief This function parses the message sent by TM robot, once entered listen node, it will initiate
@@ -47,45 +157,17 @@ class TMRobotListener {
   void finished_transfer_callback(size_t t_byte_writtened) noexcept;
 
   /**
-   * @brief
-   *
+   * @brief This function gets called if disconnect event happened, see ListenerHandle::handle_disconnect
    */
   void disconnected_callback() noexcept;
 
   /**
-   * @brief Get all the plugin object
+   * @brief This class make suitable responses for each packet type. It is basically a static_visitor that operates on
+   *        ListenData
    */
-  TMTaskHandlerArray_t get_all_plugins();
-
-  ros::NodeHandle private_nh_{"~/"};
-  pluginlib::ClassLoader<ListenerHandle> class_loader_{"tmr_listener", "tmr_listener::ListenerHandle"};
-
-  TMTaskHandler default_task_handler_{boost::make_shared<ScriptExitHandler>()};
-  TMTaskHandlerArray_t task_handlers_{};
-  TMTaskHandler current_task_handler_{};
-
-  friend struct PacketVisitor;
   struct PacketVisitor;
-
- public:
-  using ListenData = boost::variant<TMSCTPacket, TMSTAPacket, CPERRPacket>;
-  static ListenData parse(std::string t_input);
-
-  static constexpr auto DEFAULT_IP_ADDRESS = "192.168.1.2";
-  static constexpr auto LISTENER_PORT      = 5890;
-
-  explicit TMRobotListener(std::string const &t_ip_addr = DEFAULT_IP_ADDRESS) noexcept
-    : tcp_comm_{TMRobotTCP::Callback{boost::bind(&TMRobotListener::receive_tm_msg_callback, this, _1),
-                                     boost::bind(&TMRobotListener::finished_transfer_callback, this, _1),
-                                     boost::bind(&TMRobotListener::disconnected_callback, this)},
-                LISTENER_PORT, t_ip_addr},
-      task_handlers_{get_all_plugins()} {}
-
-  /**
-   * @brief This function is the entry point to the TCP/IP connection, it initiates the thread loop and runs io services
-   *        in the background
-   */
-  void start();
+  // struct InListenNodeVisitor
+  // struct OutListenNodeVisitor
 };
 
 }  // namespace tmr_listener
--- src/tmr_listener/tmr_listener.cpp
+++ src/tmr_listener/tmr_listener.cpp
@@ -16,10 +16,22 @@ inline auto strip_crlf(std::string const &t_input) noexcept {
 
 namespace tmr_listener {
 
-struct TMRobotListener::PacketVisitor final : public boost::static_visitor<void> {
-  TMRobotListener *context_holder_;
+struct TMRobotListener::PacketVisitor final : public boost::static_visitor<> {
+  /**
+   * @note Every object passed as a raw pointer (or iterator) is assumed to be owned by the caller, so that its lifetime
+   *       is handled by the caller. Viewed another way: ownership transferring APIs are relatively rare compared to
+   *       pointer-passing APIs, so the default is "no ownership transfer."
+   */
+  TMRobotTCP *tcp_comm_;
+  TMTaskHandler &current_task_handler_;
+  TMRPluginManagerBase const *const plugin_manager_;
 
-  explicit PacketVisitor(TMRobotListener *t_ctx) : boost::static_visitor<void>(), context_holder_{t_ctx} {}
+  explicit PacketVisitor(TMRobotTCP *t_tcp_comm, TMTaskHandler &t_current_task_handler_,
+                         TMRPluginManagerBase const *const t_plugin_manager)
+    : boost::static_visitor<>(),
+      tcp_comm_{t_tcp_comm},
+      current_task_handler_{t_current_task_handler_},
+      plugin_manager_{t_plugin_manager} {}
 
   template <typename T>
   void operator()(T const &) {
@@ -29,9 +41,20 @@ struct TMRobotListener::PacketVisitor final : public boost::static_visitor<void>
 
 template <>
 void TMRobotListener::PacketVisitor::operator()(CPERRPacket const &t_packet) {
-  this->context_holder_->current_task_handler_->handle_response(CPERRResponse{t_packet.data_.err_});
+  if (this->current_task_handler_) {
+    this->current_task_handler_->handle_response(CPERRResponse{t_packet.data_.err_});
+    if (t_packet.data_.err_ == ErrorCode::NotInListenNode) {
+      this->current_task_handler_.reset();
+    }
+  }
 }
 
+/**
+ * @note  Maybe I will implement TMSTAPacket service in the future, by then, these if looks even more redundant cause it
+ *        can only prevent us from using null pointer. Not like the else part can be used for other situation. For the
+ *        situation metioned above, we can only determine the decision via id, where service have specific ID created
+ *        by us
+ */
 template <>
 void TMRobotListener::PacketVisitor::operator()(TMSTAPacket const &t_packet) {
   boost::apply_visitor(*this, t_packet.data_.resp_);
@@ -39,14 +62,18 @@ void TMRobotListener::PacketVisitor::operator()(TMSTAPacket const &t_packet) {
 
 template <>
 void TMRobotListener::PacketVisitor::operator()(TMSTAPacket::DataFrame::Subcmd00Resp const &t_packet) {
-  TMSTAResponse::Subcmd00 resp{boost::get<0>(t_packet), boost::get<1>(t_packet)};
-  this->context_holder_->current_task_handler_->handle_response(resp);
+  if (this->current_task_handler_) {
+    TMSTAResponse::Subcmd00 resp{boost::get<0>(t_packet), boost::get<1>(t_packet)};
+    this->current_task_handler_->handle_response(resp);
+  }
 }
 
 template <>
 void TMRobotListener::PacketVisitor::operator()(TMSTAPacket::DataFrame::Subcmd01Resp const &t_packet) {
-  TMSTAResponse::Subcmd01 resp{boost::get<0>(t_packet), boost::get<1>(t_packet)};
-  this->context_holder_->current_task_handler_->handle_response(resp);
+  if (this->current_task_handler_) {
+    TMSTAResponse::Subcmd01 resp{boost::get<0>(t_packet), boost::get<1>(t_packet)};
+    this->current_task_handler_->handle_response(resp);
+  }
 }
 
 /**
@@ -59,38 +86,30 @@ void TMRobotListener::PacketVisitor::operator()(TMSTAPacket::DataFrame::Subcmd01
  *          generate the response, sending ScriptExit() immediately to TM robot.
  *
  * @note    TM robot will send OK message even after ScriptExit()
+ *
+ * @todo    I would like to take if else away from these operator()s
  */
 template <>
 void TMRobotListener::PacketVisitor::operator()(TMSCTPacket const &t_tmsct) {
-  TMRobotListener *&ctx = this->context_holder_;
-
   auto const server_response = boost::get<TMSCTPacket::DataFrame::ServerResponse>(t_tmsct.data_.cmd_);
-  if (not ctx->current_task_handler_) {
+  if (not this->current_task_handler_) {
     if (t_tmsct.data_.id_ == TMRobotListener::TMR_INIT_MSG_ID) {
       auto const response_content = boost::get<std::string>(server_response);
       ROS_INFO_STREAM("In Listener node, node message: " << response_content);
-      auto const predicate = [&response_content](auto const &t_handler) {
-        return t_handler->start_task_handling(response_content) == Decision::Accept;
-      };
-      // maybe we should issue warning if not only one handler satisfy the predicate
-      auto const matched = std::find_if(ctx->task_handlers_.begin(), ctx->task_handlers_.end(), predicate);
-      auto const cmd     = [&]() {
-        if (matched != ctx->task_handlers_.end()) {
-          ctx->current_task_handler_ = *matched;
-          return ctx->current_task_handler_->generate_request();
-        }
 
-        ROS_WARN_NAMED("tm_listener_node", "tm_listener_node doesn't find any handler satisfies the condition.");
-        return ctx->default_task_handler_->generate_request();
-      }();
+      auto const find_result  = this->plugin_manager_->find_task_handler(response_content);
+      auto const response_cmd = find_result->generate_request();
+      if (not response_cmd->has_script_exit()) {  // may be default handler, reset immediately
+        this->current_task_handler_ = find_result;
+      }
 
-      ROS_INFO_STREAM_NAMED("tm_listen_node", "Write msg: " << ::strip_crlf(cmd->to_str()));
-      ctx->tcp_comm_.write(cmd->to_str());
+      ROS_INFO_STREAM_NAMED("tm_listen_node", "Write msg: " << ::strip_crlf(response_cmd->to_str()));
+      this->tcp_comm_->write(response_cmd->to_str());
     }
   } else {
     auto const response_content = boost::get<TMSCTPacket::DataFrame::ScriptResult>(server_response);
     TMSCTResponse const resp{t_tmsct.data_.id_, response_content.result_, std::move(response_content.abnormal_lines_)};
-    ctx->current_task_handler_->handle_response(resp);
+    this->current_task_handler_->handle_response(resp);
   }
 }
 
@@ -98,66 +117,83 @@ void TMRobotListener::PacketVisitor::operator()(TMSCTPacket const &t_tmsct) {
 
 namespace tmr_listener {
 
+TMTaskHandlerArray_t RTLibPluginManager::load_plugins() {
+  using boost::adaptors::transformed;
+
+  auto const plugin_transformer = [this](auto const &t_name) { return class_loader_.createInstance(t_name); };
+  auto const plugin_names       = ros::param::param("/tmr_listener/listener_handles", std::vector<std::string>{});
+  ROS_DEBUG_STREAM_NAMED("tmr_plugin_manager", "plugin num: " << plugin_names.size());
+
+  auto const plugins = plugin_names | transformed(plugin_transformer);
+  return TMTaskHandlerArray_t{plugins.begin(), plugins.end()};
+}
+
+TMTaskHandler RTLibPluginManager::find_task_handler(std::string const &t_input) const noexcept {
+  auto const predicate = [&t_input](auto const &t_handler) {
+    return t_handler->start_task_handling(t_input) == Decision::Accept;
+  };
+
+  auto const matched = std::find_if(this->all_plugins_.begin(), this->all_plugins_.end(), predicate);
+  if (matched == this->all_plugins_.end()) {
+    ROS_WARN_STREAM_NAMED("tmr_plugin_manager", "No suitable handler to take care of current task, returning default");
+    return this->default_handler_;
+  }
+
+  return *matched;
+}
+
+/**
+ * @note  Even though I can make it a static class variable, I decided to create local static to make it
+ *        similar to other parsing rule
+ */
+TMRobotListener::TMRListenDataParser TMRobotListener::parsing_rule() {
+  static TMRListenDataParser parser = TMSCTPacket::parsing_rule() | TMSTAPacket::parsing_rule() |  //
+                                      CPERRPacket::parsing_rule();
+  return parser;
+}
+
+/**
+ * @todo throw if not fully matched
+ */
 TMRobotListener::ListenData TMRobotListener::parse(std::string t_input) {
   using namespace boost::spirit::qi;
   using boost::spirit::ascii::space;
 
-  static ParseRule<ListenData> parse_rule = TMSCTPacket::parsing_rule() | TMSTAPacket::parsing_rule() |  //
-                                            CPERRPacket::parsing_rule();
-
   ListenData ret_val;
-  bool const res = phrase_parse(t_input.begin(), t_input.end(), parse_rule, space, ret_val);
+  bool const res = phrase_parse(t_input.begin(), t_input.end(), parsing_rule(), space, ret_val);
 
   return ret_val;
 }
 
 /**
  * @todo  We don't need to parse the input in one step, maybe parse the input in mulitple step, see TMSVR parsing
- * @todo  If ScriptExit() happened, this->current_task_handler_ will reset, result in bad_get for not-yet-responded
- *        message. Maybe the ID of the message needs to be traced. Not sure.
  */
 void TMRobotListener::receive_tm_msg_callback(std::string const &t_input) noexcept {
   ROS_INFO_STREAM("Received: " << ::strip_crlf(t_input));
 
   auto const result = parse(t_input);
-  auto visitor      = PacketVisitor{this};
+  auto visitor      = PacketVisitor{&this->tcp_comm_, this->current_task_handler_, this->plugin_manager_.get()};
   boost::apply_visitor(visitor, result);
 }
 
-/**
- * @details
- */
 void TMRobotListener::finished_transfer_callback(size_t const /*t_byte_writtened*/) noexcept {
   if (this->current_task_handler_) {
     auto const cmd = this->current_task_handler_->generate_request();
-    auto const str = cmd->to_str();
     if (cmd->has_script_exit()) {
       this->current_task_handler_.reset();
     }
 
+    auto const str = cmd->to_str();
     ROS_INFO_STREAM_COND_NAMED(not str.empty(), "tm_listen_node", "Write msg: " << ::strip_crlf(str));
     this->tcp_comm_.write(str);
   }
 }
 
-/**
- * @details
- */
 void TMRobotListener::disconnected_callback() noexcept {
   if (this->current_task_handler_) {
     this->current_task_handler_->handle_disconnect();
     this->current_task_handler_.reset();
   }
-}
-
-TMRobotListener::TMTaskHandlerArray_t TMRobotListener::get_all_plugins() {
-  using boost::adaptors::transformed;
-  auto const plugin_transformer = [this](auto const &t_name) { return this->class_loader_.createInstance(t_name); };
-  auto const plugin_names       = this->private_nh_.param("listener_handles", std::vector<std::string>{});
-  ROS_DEBUG_STREAM_NAMED("tmr_listener", "plugin num: " << plugin_names.size());
-
-  auto const plugins = plugin_names | transformed(plugin_transformer);
-  return TMTaskHandlerArray_t{plugins.begin(), plugins.end()};
 }
 
 void TMRobotListener::start() {
